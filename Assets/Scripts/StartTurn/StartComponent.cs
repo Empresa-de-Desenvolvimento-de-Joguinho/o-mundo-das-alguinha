a54077d2ad51d1a24118328952564247948cd4ce
marvintomizawa <marvintomizawa@hotmail.com>
2021-03-16 02:51:39 -0300

    FInalizado componente de inicio de turno

--- Assets/Scripts/StartTurn/StartComponent.cs
+++ Assets/Scripts/StartTurn/StartComponent.cs
@@ -1,20 +1,31 @@
 ﻿using Jogador;
+using TMPro;
 using Turno;
+using UnityEngine;
 
 public class StartComponent: TurnComponent
 {
+
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private TextMeshProUGUI titleText;
+
+    private IPlayerDetails currentPlayer;
+
     public void SetPlayer(IPlayerDetails player)
     {
+        currentPlayer = player;
+        var playeyName = currentPlayer.GetCharacter().GetName();
 
+        titleText.text = $"{playeyName}, é o seu turno!!";
     }
 
     public override void ShowElements()
     {
-        throw new System.NotImplementedException();
+        canvas.SetActive(true);
     }
 
     protected override void HideComponent()
     {
-        throw new System.NotImplementedException();
+        canvas.SetActive(false);
     }
 }
