dacefb587c6f1f82685ba0f49f4caffaaeccfcb5
Si3gh <jp.b.pissolito@gmail.com>
2021-03-16 22:48:02 -0300

    Implementação carta de perguntas

--- Assets/Scripts/Cartas/Question.cs
+++ Assets/Scripts/Cartas/Question.cs
@@ -1,9 +1,11 @@
 using System;
+using UnityEngine;
 
 namespace Cartas
 {	
 	[Serializable]
-	public class Question
+	[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Question", order = 2)]
+	public class Question : ScriptableObject
 	{
 		public string fact;
 		public bool isTrue;
--- Assets/Scripts/Cartas/QuestionManager.cs
+++ Assets/Scripts/Cartas/QuestionManager.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
-using System.Security.Cryptography;
+using TMPro;
 using Turno;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,27 +20,27 @@ namespace Cartas
 		// public Question[] PollutedQuestions;
 		// public Question[] BentonicQuestions;
 		// public Question[] CoastQuestions;
-		public Question[] questions;
-		private static List<Question> unansweredQuestions;
-		private Question currentQuestion;
 
-		[SerializeField] private Text factText;
-		// void Start()
-		// {
-		// 	if (unansweredQuestions == null || unansweredQuestions.Count == 0)
-		// 	{
-		// 		unansweredQuestions = questions.ToList<Question>();
-		// 	}
-		// 	GetRandomQuestion();
-		// }
+		[SerializeField] public Question[] questions;
+		[SerializeField] private TextMeshProUGUI factText;
+		[SerializeField] private GameObject canvas;
+		[SerializeField] private GameObject deck;
+		[SerializeField] private GameObject cardScreen;
+		
+		[SerializeField]
+		private List<Question> _unansweredQuestions;
+		private Question _currentQuestion;
+		private int _quantityToMove;
+
+		// [SerializeField] private float timeBetweenQuestions = 2f;
+
 
 		void SetCurrentQuestion()
 		{
-			int randomQuestionIndex = new Random().NextInt(0, unansweredQuestions.Count);
-			currentQuestion = unansweredQuestions[randomQuestionIndex];
+			int randomQuestionIndex = new Random().NextInt(0, _unansweredQuestions.Count);
+			_currentQuestion = _unansweredQuestions[randomQuestionIndex];
 
-			factText.text = currentQuestion.fact;
-			unansweredQuestions.RemoveAt(randomQuestionIndex);
+			factText.text = _currentQuestion.fact;
 		}
 
 		/*
@@ -52,52 +50,50 @@ namespace Cartas
 		/*
 		 * TODO - Falta importar métodos a objetos na Unity && 
 		 */
-		public void UserSelectTrue()
+
+		public void ValidateQuestion(bool answer) // true false
 		{
-			if (currentQuestion.isTrue)
+			if (_currentQuestion.isTrue == answer)
 			{
 				Debug.Log(("CERTO"));
+				_unansweredQuestions.Remove(_currentQuestion);
+				_quantityToMove = 1;
 			}
 			else
 			{
 				Debug.Log(("ERRADO"));
+				_unansweredQuestions.Remove(_currentQuestion);
+				_quantityToMove = -1;
 			}
 		}
-		
-		public void UserSelectFalse()
+
+		public void ShowCardScreen()
 		{
-			if (!currentQuestion.isTrue)
-			{
-				Debug.Log(("CERTO"));
-			}
-			else
-			{
-				Debug.Log(("ERRADO"));
-			}
+			deck.SetActive(false);
+			cardScreen.SetActive(true);
 		}
 
 		public override void ShowElements()
 		{
-			if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+			if (_unansweredQuestions == null || _unansweredQuestions.Count == 0)
 			{
-				unansweredQuestions = questions.ToList<Question>();
+				_unansweredQuestions = questions.ToList<Question>();
 			}
 
 			SetCurrentQuestion();
-			throw new System.NotImplementedException();
+			canvas.SetActive(true);
+			deck.SetActive(true);
 		}
 
 		protected override void HideComponent()
 		{
-			throw new System.NotImplementedException();
+			canvas.SetActive(false);
+			cardScreen.SetActive(false);
 		}
 
 		public override int GetQuantityToMove()
 		{
-			/*TODO
-			 * A função GetQuantityToMove deverá retornar +1 ou -1 dependendo se o jogador acertou ou errou a pergunta
-			 */
-			throw new System.NotImplementedException();
+			return _quantityToMove;
 		}
 	}
 }
