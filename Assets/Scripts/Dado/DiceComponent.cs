79ebb15fe637e2931cf46eabae0a8b47e7198bba
vito <viitorrsbarbosa@gmail.com>
2021-03-17 21:53:04 -0300

    Adicionar comportamento do Rolamento de dados

--- Assets/Scripts/Dado/DiceComponent.cs
+++ Assets/Scripts/Dado/DiceComponent.cs
@@ -5,15 +5,58 @@ using Random = System.Random;
 
 namespace Turno
 {
-    public abstract class DiceComponent : TurnComponent
-    {
-        [SerializeField] [Range(1, 6)] private int diceSide;
-        private Random rnd = new Random();
+	public class DiceComponent : MoveComponent
+	{
+		[SerializeField] [Range(1, 6)] private int diceSide;
+		private bool canRollDice = true;
+		private Animator animator;
+		private Random rnd = new Random();
 
-        public int RollDice()
-        {
-            diceSide = rnd.Next();
-            return diceSide;
-        }
-    }
+		public override int GetQuantityToMove()
+		{
+			Start();
+			return diceSide;
+		}
+		public void RollDice()
+		{
+			if (canRollDice)
+			{
+				diceSide = rnd.Next();
+				showDiceSide(diceSide);
+				canRollDice = false;
+			}
+			throw new System.Exception("It's not possible to roll the dice now.");
+		}
+
+		public override void ShowElements()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		protected override void HideComponent()
+		{
+			throw new System.NotImplementedException();
+		}
+		void showDiceSide(int diceSide)
+		{
+			if (diceSide.Equals(1))
+				animator.Play("dado_rolando_01");
+			if (diceSide.Equals(2))
+				animator.Play("dado_rolando_02");
+			if (diceSide.Equals(3))
+				animator.Play("dado_rolando_03");
+			if (diceSide.Equals(4))
+				animator.Play("dado_rolando_04");
+			if (diceSide.Equals(5))
+				animator.Play("dado_rolando_05");
+			if (diceSide.Equals(6))
+				animator.Play("dado_rolando_06");
+		}
+
+		private void Start()
+		{
+			animator = gameObject.GetComponent<Animator>();
+			canRollDice = true;
+		}
+	}
 }
---
+++ Assets/Scripts/Turno/Carta/QuestionComponent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Turno;
+using UnityEngine;
+
+public class QuestionComponent : MoveComponent
+{
+	[SerializeField] private List<Question> questions;
+	[SerializeField] private Question activeQuestion;
+	[SerializeField] private bool wasAnsweredRight;
+
+	public void GetQuestion()
+    {
+
+    }
+
+	public void ShowQuestion()
+    {
+
+    }
+	public override int GetQuantityToMove()
+	{
+
+		return 1;
+	}
+
+	public override void ShowElements()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	protected override void HideComponent()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private class Question
+	{
+
+	}
+}
